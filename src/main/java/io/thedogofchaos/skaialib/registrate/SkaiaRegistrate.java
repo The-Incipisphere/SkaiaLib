013dd0abd0997f96c7d2487cefd821a85ec1edae
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-02-07 20:57:05 +0000

    dead-basic registrate extension class
    Signed-off-by: Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>

---
+++ src/main/java/io/thedogofchaos/skaialib/registrate/SkaiaRegistrate.java
@@ -0,0 +1,9 @@
+package io.thedogofchaos.skaialib.registrate;
+
+import com.tterrag.registrate.Registrate;
+
+public class SkaiaRegistrate extends Registrate {
+    protected SkaiaRegistrate(String modid) {
+        super(modid);
+    }
+}
