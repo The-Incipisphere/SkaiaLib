928dc9ccba2245b5f6d319e37255b1182b12aa58
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2025-02-08 01:57:25 +0000

    licensing (HOPEFULLY NOT DOUBLED) (SDHNASNDJKSA)
    Signed-off-by: Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>

--- src/main/java/io/thedogofchaos/skaialib/common/CommonProxy.java
+++ src/main/java/io/thedogofchaos/skaialib/common/CommonProxy.java
@@ -1,40 +1,4 @@
-/*
- * Copyright (C) 2025 The-Incipisphere
- *
- * This file is part of SkaiaLib.
- *
- * SkaiaLib is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SkaiaLib is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SkaiaLib. If not, see <http://www.gnu.org/licenses/>.
- */
 
-/*
- * Copyright (C) 2025 The-Incipisphere
- *
- * This file is part of SkaiaLib.
- *
- * SkaiaLib is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SkaiaLib is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with SkaiaLib. If not, see <http://www.gnu.org/licenses/>.
- */
 
 package io.thedogofchaos.skaialib.common;
 
